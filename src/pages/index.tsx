58eb3181b1d468c1393dbbd01d887abefaca0d85
Renan.Correa <renan.rcp16@gmail.com>
2022-08-30 10:16:16 -0300

    starter components created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className={`
+		flex h-screen justify-center items-center
+		bg-gradient-to-r from-blue-500 to-purple-500
+		text-white
+	`}>
+		<Layout title="Cadastro Simples">
+			<span>Conteúdo</span>
+		</Layout>
+	</div>
   )
 }
